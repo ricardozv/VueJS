914607ab293bf3c5e3d291e08af0f623f5ceccb6
ricardozv <ricardosilvanet03@gmail.com>
2021-03-17 02:25:44 -0300

    clean code

--- cybersec/src/App.vue
+++ cybersec/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+ 
+  <h1> Cyber Segurança </h1>
+
+ </div>
+ <Menu/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
